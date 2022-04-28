aa5bd588173b4a8a4f25d46a40a0afab1870a0c2
Shanto Mathew <shanto12@gmail.com>
2022-04-28 16:14:06 +1000

    regression test for whitelisting.

--- contracts/fortune_eth.sol
+++ contracts/fortune_eth.sol
@@ -1394,4 +1394,5 @@ contract Fortune is ERC1155, Pausable, ERC1155Burnable {
     function validTokenId(uint256 _id) internal view {
         require(_id <= supplies.length && _id > 0, "Token doesn't exist");
     }
+
 }
