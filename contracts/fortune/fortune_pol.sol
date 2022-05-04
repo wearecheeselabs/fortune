462225d7bdb5577a96bdaf17956270f990c41402
Shanto Mathew <shanto12@gmail.com>
2022-05-04 14:18:00 +1000

    polygon contract changed and tested.

--- contracts/fortune/fortune_pol.sol
+++ contracts/fortune/fortune_pol.sol
@@ -11,26 +11,29 @@ contract FortunePol is Pausable, ERC1155Burnable {
 
     // Change all these arrays to mapping 
     // we might need to mint another tokenId in the nearest future
-    
-    uint256[] supplies = [3, 3, 3];
-    uint256[] minted = [0, 0, 0];
-    uint256[] WhitelistCount = [0, 0, 0];
+
+    //    uint256[] supplies = [3, 3, 3];
+    //    uint256[] minted = [0, 0, 0];
+    //    uint256[] WhitelistCount = [0, 0, 0];
 
     // Change the arrays to mapping
-	// mapping(uint256 => uint256) public supplies; // tokenId => supply value
-	// mapping(uint256 => uint256) public minted; // tokenId => minted amount
-	// mapping(uint256 => uint256) private rates; // tokenId => rate value
-	// mapping(uint256 => uint256) private whitelistCount;
+    mapping(uint256 => uint256) public supplies; // tokenId => supply value
+    mapping(uint256 => uint256) public minted; // tokenId => minted amount
+//    mapping(uint256 => uint256) private rates; // tokenId => rate value: We don't have rates for this mint. It is free mint by us for the users.
+    mapping(uint256 => uint256) private whitelistCount;
 
     mapping(uint256 => string) public tokenURI;
     mapping(address => uint256) public whitelist;
-
-    //    event Log(string msg, address _id, uint count, uint addressvalue);
 
     constructor() ERC1155("") {
         name = name;
         symbol = symbol;
         owner = msg.sender;
+        for (uint256 _id = 1; _id <= 3; _id++) {
+            supplies[_id]=3;
+//            minted[_id]=0;
+//            whitelistCount[_id]=0;
+        }
     }
 
     function setURI(uint256 _id, string memory _uri) external {
@@ -64,96 +67,94 @@ contract FortunePol is Pausable, ERC1155Burnable {
 
     // Add when not pause check here
     // You don't want anyone to mint when the contract is paused
+    // NSOG: This is an internal function and called from mitAll which can only by called by the owner whenNotPaused() is there. We don't expect users to mint this. We mint it for them.
     function mint(address _to) internal {
         onlyWhitelistAddress(_to);
         uint256 _id = isWhitelisted(_to);
-        require(_id <= supplies.length && _id > 0, "Token doesn't exist");
-        require(minted[_id - 1] + 1 <= supplies[_id - 1], "Not enough supply");
+        require(minted[_id] + 1 <= supplies[_id], "Not enough supply");
         _mint(_to, _id, 1, "");
         // Why id -1?
-        minted[_id - 1]++;
+        minted[_id]++;
         whitelist[_to] = 99;
     }
 
-    // Add when not pause check here
+    // Add when not pause check here: Done
     // You don't want anyone to burn token when the contract is paused
-    // Add who can burn here, only creator as the current token owner
+    // Add who can burn here, only creator as the current token owner: Added code that only current token owner can burn it.
     // or only admin  should be able to burn
     function burn(uint256 _id, uint256 _amount) external {
+        whenNotPaused();
+        onlyTokenOwner(_id);
         _burn(msg.sender, _id, _amount);
     }
 
-    // Add when not pause check here
+    // Add when not pause check here: Done
     // You don't want anyone to burn token when the contract is paused
-    // Add who can burn here, only creator as the current token owner
+    // Add who can burn here, only creator as the current token owner: Implemented that only the token owner can burn.
     // or only admin  should be able to burn
     function burnBatch(uint256[] memory _ids, uint256[] memory _amounts)
-        external
+    external
     {
+        whenNotPaused();
+        for (uint256 i = 0; i < _ids.length; i++) {
+            onlyTokenOwner(_ids[i]);
+        }
         _burnBatch(msg.sender, _ids, _amounts);
     }
 
-    // What is the use of this function?
-    // Why are you burning same id and minting same ids to the same address?
-    function burnForMint(
-        address _from,
-        uint256[] memory _burnIds,
-        uint256[] memory _burnAmounts,
-        uint256[] memory _mintIds,
-        uint256[] memory _mintAmounts
-    ) external {
-        onlyOwner();
-        _burnBatch(_from, _burnIds, _burnAmounts);
-        _mintBatch(_from, _mintIds, _mintAmounts, "");
-    }
+//    // What is the use of this function?. Burn and mint in the same single function
+//    // Why are you burning same id and minting same ids to the same address?: It is not the same IDs. burning _burnIds and minting _mintIds. Not so useful function. Can remove if you think it is not required.
+//    function burnForMint(
+//        address _from,
+//        uint256[] memory _burnIds,
+//        uint256[] memory _burnAmounts,
+//        uint256[] memory _mintIds,
+//        uint256[] memory _mintAmounts
+//    ) external {
+//        onlyOwner();
+//        _burnBatch(_from, _burnIds, _burnAmounts);
+//        _mintBatch(_from, _mintIds, _mintAmounts, "");
+//    }
 
     function uri(uint256 _id) public view override returns (string memory) {
         return tokenURI[_id];
     }
 
-    function _beforeTokenTransfer(
-        address operator,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal override(ERC1155)  {
-        whenNotPaused();
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-    }
+//    function _beforeTokenTransfer(
+//        address operator,
+//        address from,
+//        address to,
+//        uint256[] memory ids,
+//        uint256[] memory amounts,
+//        bytes memory data
+//    ) internal override(ERC1155) {
+//        whenNotPaused();
+//        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+//    }
 
     function batchWhitelistAddress(address[] memory _addresses, uint256 _id)
-        public
-    //    onlyOwner
-    //    validTokenId (_id)
+    public
     {
         onlyOwner();
         validTokenId(_id);
         uint256 count = 0;
         for (uint256 i = 0; i < _addresses.length; i++) {
-            //            emit Log("in for loop", _addresses[i], count, whitelist[_addresses[i]]);
             if (whitelist[_addresses[i]] == 0) {
                 whitelist[_addresses[i]] = _id;
                 count++;
             }
-            //            else     {
-            //                emit Log("address is already whitelisted", _addresses[i], WhitelistCount[_id-1], whitelist[_addresses[i]]);
-            //            }
         }
         require(
-            // Why id -1?
-            WhitelistCount[_id - 1] + count <= supplies[_id - 1],
+        // Why id -1?: id -1 was to point to the id in the 0 indexed array. Removed it since we moved to mapping.
+            whitelistCount[_id] + count <= supplies[_id],
             "Exceed maxSupply"
         );
-        // Why id -1?
-        WhitelistCount[_id - 1] += count;
+        // Why id -1?: id -1 was to point to the id in the 0 indexed array. Removed it since we moved to mapping.
+        whitelistCount[_id] += count;
     }
 
     function batchRemoveWhitelist(address[] memory _addresses, uint256 _id)
-        public
-    //    onlyOwner
-    //    validTokenId (_id)
+    public
     {
         onlyOwner();
         validTokenId(_id);
@@ -163,11 +164,8 @@ contract FortunePol is Pausable, ERC1155Burnable {
                 whitelist[_addresses[i]] = 0;
                 count++;
             }
-            //            else     {
-            //                emit Log("Adress is not already whitelisted for this Token ID.", _addresses[i], WhitelistCount[_id-1], whitelist[_addresses[i]]);
-            //            }
         }
-        WhitelistCount[_id - 1] -= count;
+        whitelistCount[_id] -= count;
     }
 
     function isWhitelisted(address _address) public view returns (uint256) {
@@ -175,18 +173,18 @@ contract FortunePol is Pausable, ERC1155Burnable {
     }
 
     function getWhitelistCount(uint256 _id) public view returns (uint256) {
-        // Why id -1?
-        return WhitelistCount[_id - 1];
+        // Why id -1?: id -1 was to point to the id in the 0 indexed array. Removed it since we moved to mapping.
+        return whitelistCount[_id];
     }
 
     function getMintedCount(uint256 _id) public view returns (uint256) {
-        // Why id -1?
-        return minted[_id - 1];
+        // Why id -1?: id -1 was to point to the id in the 0 indexed array. Removed it since we moved to mapping.
+        return minted[_id];
     }
 
     function getMaxSupply(uint256 _id) public view returns (uint256) {
-        // Why id -1?
-        return supplies[_id - 1];
+        // Why id -1?: id -1 was to point to the id in the 0 indexed array. Removed it since we moved to mapping.
+        return supplies[_id];
     }
 
     function onlyOwner() internal view {
@@ -197,11 +195,17 @@ contract FortunePol is Pausable, ERC1155Burnable {
     }
 
     function onlyWhitelistAddress(address _to) internal view {
-        require(whitelist[_to] != 0, "Address not whitelisted. Cant mint.");
+        require(whitelist[_to] != 0, "Address not whitelisted. Can't mint.");
         require(whitelist[_to] != 99, "Address already minted!");
     }
 
     function validTokenId(uint256 _id) internal view {
-        require(_id <= supplies.length && _id > 0, "Token doesn't exist");
+//        require(_id <= supplies.length && _id > 0, "Token doesn't exist");
+        require(supplies[_id] !=0, "Token doesn't exist");
+    }
+    function onlyTokenOwner(uint256 _id) internal view {
+        require(balanceOf(msg.sender, _id) !=0 , "You are not the owner of this NFT.");
     }
 }
+
+
