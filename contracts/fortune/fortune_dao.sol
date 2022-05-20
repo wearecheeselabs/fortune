012ebcb7c0f7a9f5df6e209f2e8e6aa62478c97f
makerzy <botassammzy@gmail.com>
2022-05-20 19:05:55 -0400

    added missing files

--- contracts/fortune/fortune_dao.sol
+++ contracts/fortune/fortune_dao.sol
@@ -1,39 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "../tokens/ERC721/ERC721.sol";
+import "../tokens/ERC721/ERC721Enumerable.sol";
+import "../tokens/ERC721/ERC721URIStorage.sol";
+import "../security/Pausable.sol";
+import "../security/Ownable.sol";
+import "../tokens/ERC721/ERC721Burnable.sol";
+import "../votes/EIP712.sol";
+import "../tokens/ERC721/ERC721Votes.sol";
+
+
 
 contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable, EIP712, ERC721Votes {
     constructor() ERC721("MyToken", "MTK") EIP712("MyToken", "1") {}
 
-    function pause() public onlyOwner {
+    function pause() public  {
+        onlyOwner();
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public  {
+        onlyOwner();
         _unpause();
     }
 
     function safeMint(address to, uint256 tokenId, string memory uri)
         public
-        onlyOwner
+        
     {
+        onlyOwner();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
-        whenNotPaused
         override(ERC721, ERC721Enumerable)
     {
+        whenNotPaused();
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
